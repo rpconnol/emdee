96e87cf568e7c3084861dbb825e050ccfe36a7c2
rpconnol <rpconnol@gmail.com>
2019-01-17 15:21:53 -0500

    Clean up READMEs in tests/load_run

--- tests/load_run/load_run.py
+++ tests/load_run/load_run.py
@@ -18,6 +18,7 @@ print(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
 if os.path.exists('load_data'):
     shutil.rmtree('load_data')
 shutil.copytree('test_data','load_data')
+os.remove('load_data/README')
 
 
 emdeeClass = emdee.Emdee(mode='load',loc='load_data')
